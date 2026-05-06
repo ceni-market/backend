4f2db0d5cbdf38a8358901c291a742a554d679bc
AJH <AJH@>
2026-05-06 18:25:12 +0900

    fix : category 엔티티 필요없는 항목 수정

--- src/main/java/com/cenimarket/backend/category/domain/Category.java
+++ src/main/java/com/cenimarket/backend/category/domain/Category.java
@@ -2,33 +2,30 @@ package com.cenimarket.backend.category.domain;
 
 import com.cenimarket.backend.global.domain.BaseEntity;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
-
-import java.util.List;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "categories")
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Category extends BaseEntity {
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "parent_id")
-    private Category parent;
-
-    @Column(name = "sort_order", nullable = false)
-    private Integer sortOrder;
-
-    @OneToMany(mappedBy = "parent")
-    private List<Category> children;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "is_active", nullable = false)
-    private Boolean active;
-
-    @Column(nullable = false, length = 100)
+    @Column(nullable = false, length = 100, unique = true)
     private String name;
 
+    @Column(name = "sort_order", nullable = false)
+    private Integer sortOrder;
+
+    public static Category from(DefaultCategory defaultCategory) {
+        Category category = new Category();
+        category.name = defaultCategory.getDisplayName();
+        category.sortOrder = defaultCategory.getSortOrder();
+        return category;
+    }
 }
