feb39ef74577a743d3cfab6c91b5ea2e35a685ab
dookh99 <dookh1999@gmail.com>
2026-05-06 17:44:29 +0900

    Repository 추가. DTO 일부 추가. WebSocket 의존성 추가

--- src/main/java/com/cenimarket/backend/chat/dto/response/ChatRoomResponse.java
+++ src/main/java/com/cenimarket/backend/chat/dto/response/ChatRoomResponse.java
@@ -1,11 +1,4 @@
 package com.cenimarket.backend.chat.dto.response;
 
-import lombok.*;
-
-@Getter
-@Setter
-@ToString
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ChatRoomResponse {
-
 }
