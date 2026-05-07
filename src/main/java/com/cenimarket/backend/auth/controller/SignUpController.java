38ddd697cb10774e2b9e7da2791c56dbbb6e3e73
suhwan <suhwan1117@gmail.com>
2026-05-07 10:59:55 +0900

    feat: 회원가입 전체

--- src/main/java/com/cenimarket/backend/auth/controller/SignUpController.java
+++ src/main/java/com/cenimarket/backend/auth/controller/SignUpController.java
@@ -3,6 +3,7 @@ package com.cenimarket.backend.auth.controller;
 import com.cenimarket.backend.auth.service.SignUpService;
 import com.cenimarket.backend.auth.dto.request.SignUpRequestDTO;
 import com.cenimarket.backend.auth.dto.response.SignUpResponseDTO;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,14 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/auth")
 public class SignUpController {
     private final SignUpService signUpService;
 
     /*회원가입 API*/
 
     @PostMapping("/signup")
-    public ResponseEntity<SignUpResponseDTO> signUp(@RequestBody SignUpRequestDTO requestDTO) {
+    public ResponseEntity<SignUpResponseDTO> signUp(@Valid @RequestBody SignUpRequestDTO requestDTO) {
         // 서비스 계층의 가입 로직 호출
         SignUpResponseDTO response = signUpService.signUp(requestDTO);
 
