0b92e1cd5aa85b9c84e949a5738b4ab8ae0068b2
AJH <AJH@>
2026-05-07 18:27:06 +0900

    feat : 게시글등록 dto 추가

---
+++ src/main/java/com/cenimarket/backend/listing/dto/request/ListingCreateRequest.java
@@ -0,0 +1,23 @@
+package com.cenimarket.backend.listing.dto.request;
+
+import com.cenimarket.backend.listing.domain.ListingType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ListingCreateRequest {
+    private Long sellerId;
+    private Long categoryId;
+    private String title;
+    private String description;
+    private Integer price;
+    private ListingType type;
+    private List<String> images;
+
+}
