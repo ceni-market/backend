e8dba6c0856cc29a52bf5920d303c68bb2d4afb1
suhwan <suhwan1117@gmail.com>
2026-05-08 15:42:11 +0900

    feat: 여러가지

--- src/main/java/com/cenimarket/backend/global/security/SecurityConfig.java
+++ src/main/java/com/cenimarket/backend/global/security/SecurityConfig.java
@@ -31,6 +31,14 @@ public class SecurityConfig {
                 // 3. 권한 설정
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll() // 로그인, 회원가입 관련은 인증 없이 허용
+                        // 2. ⭐ Swagger 관련 모든 경로 허용 (토큰 없이 접근 가능)
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/swagger-resources/**",
+                                "/webjars/**"
+                        ).permitAll()
+                        .requestMatchers("/chatroom").permitAll()
                         //.requestMatchers("/**").permitAll() // 일단 다되게 만듬
                         .anyRequest().authenticated()               // 그 외 모든 요청은 인증(토큰) 필요
                 )
