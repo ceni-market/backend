3f93ac29a7fd67381e2cb135596abfbbcfd7749b
AJH <AJH@>
2026-05-08 10:32:12 +0900

    feat : 게시글 등록 작성완료

---
+++ src/main/java/com/cenimarket/backend/listing/controller/ListingController.java
@@ -0,0 +1,30 @@
+package com.cenimarket.backend.listing.controller;
+
+import com.cenimarket.backend.global.response.ApiResponse;
+import com.cenimarket.backend.listing.dto.request.ListingCreateRequest;
+import com.cenimarket.backend.listing.dto.response.ListingCreateResponse;
+import com.cenimarket.backend.listing.service.ListingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/listings")
+public class ListingController {
+
+    private final ListingService listingService;
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<ListingCreateResponse>> createListing(
+            @RequestBody
+            ListingCreateRequest request
+    ) {
+        ListingCreateResponse response =listingService.createListing(request);
+        return ResponseEntity.ok(ApiResponse.ok(response));
+    }
+
+}
