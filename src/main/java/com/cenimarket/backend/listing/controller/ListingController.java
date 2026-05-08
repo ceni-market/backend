fd7cf7c9afb572ac689b1145e8480e3d991d94fa
AJH <AJH@>
2026-05-08 14:24:07 +0900

    fix: 게시글 상대 변경기능추가

--- src/main/java/com/cenimarket/backend/listing/controller/ListingController.java
+++ src/main/java/com/cenimarket/backend/listing/controller/ListingController.java
@@ -2,9 +2,11 @@ package com.cenimarket.backend.listing.controller;
 
 import com.cenimarket.backend.global.response.ApiResponse;
 import com.cenimarket.backend.listing.dto.request.ListingCreateRequest;
+import com.cenimarket.backend.listing.dto.request.ListingStatusUpdateRequest;
 import com.cenimarket.backend.listing.dto.request.ListingUpdateRequest;
 import com.cenimarket.backend.listing.dto.response.ListingCreateResponse;
 import com.cenimarket.backend.listing.dto.response.ListingDeleteResponse;
+import com.cenimarket.backend.listing.dto.response.ListingStatusUpdateResponse;
 import com.cenimarket.backend.listing.dto.response.ListingUpdateResponse;
 import com.cenimarket.backend.listing.service.ListingService;
 import jakarta.validation.Valid;
@@ -18,7 +20,7 @@ import org.springframework.web.bind.annotation.*;
 public class ListingController {
 
     private final ListingService listingService;
-
+    //게시글 생성
     @PostMapping
     public ResponseEntity<ApiResponse<ListingCreateResponse>> createListing(
             @Valid
@@ -28,7 +30,7 @@ public class ListingController {
         ListingCreateResponse response =listingService.createListing(request);
         return ResponseEntity.ok(ApiResponse.ok(response));
     }
-
+    //게시글 수정
     @PutMapping("/{listingId}")
     public ResponseEntity<ApiResponse<ListingUpdateResponse>> updateListing(
             @PathVariable Long listingId,
@@ -37,12 +39,21 @@ public class ListingController {
         ListingUpdateResponse response = listingService.updateListing(listingId, request);
         return ResponseEntity.ok(ApiResponse.ok(response));
     }
-
+    //게시글 삭제
     @DeleteMapping("/{listingId}")
     public ResponseEntity<ApiResponse<ListingDeleteResponse>> deleteListing(
             @PathVariable Long listingId
     ){
         ListingDeleteResponse response = listingService.deleteListing(listingId);
+        return ResponseEntity.ok(ApiResponse.ok(response));
+    }
+    //게시글 상태변경
+    @PatchMapping("/{listingId}/status")
+    public ResponseEntity<ApiResponse<ListingStatusUpdateResponse>> updateListingStatus(
+            @PathVariable Long listingId,
+            @Valid @RequestBody ListingStatusUpdateRequest request
+    ){
+        ListingStatusUpdateResponse response = listingService.updateListingStatus(listingId, request);
         return ResponseEntity.ok(ApiResponse.ok(response));
     }
 }
