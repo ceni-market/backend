45129f2ea0d4a27f5564eee0d52ae550cc113f62
doo <dookh1999@kyonggi.ac.kr>
2026-05-10 20:02:24 +0900

    stomp 채팅 기능, 채팅방(채팅멤버)생성, 채팅 저장 기능 추가.

--- src/main/java/com/cenimarket/backend/chat/dto/response/ChatRoomResponse.java
+++ src/main/java/com/cenimarket/backend/chat/dto/response/ChatRoomResponse.java
@@ -1,4 +1,11 @@
 package com.cenimarket.backend.chat.dto.response;
 
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ChatRoomResponse {
+
 }
