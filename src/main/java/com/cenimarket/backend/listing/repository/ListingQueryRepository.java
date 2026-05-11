b0006a18ffe2936cf819358862d5cec79c4c07aa
Gorani77 <sp2877@naver.com>
2026-05-11 12:25:42 +0900

    feat: 게시글 상세 조회

--- src/main/java/com/cenimarket/backend/listing/repository/ListingQueryRepository.java
+++ src/main/java/com/cenimarket/backend/listing/repository/ListingQueryRepository.java
@@ -4,4 +4,5 @@ import com.cenimarket.backend.listing.domain.Listing;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ListingQueryRepository extends JpaRepository<Listing, Long> {
+    Listing findListingById(Long id);
 }
