0e86b51669266bf698454f639d2ebdd1a8b638ba
Gorani77 <sp2877@naver.com>
2026-05-11 10:38:39 +0900

    feat: 리스트 조회

--- src/main/java/com/cenimarket/backend/listing/dto/response/ListingsListResponse.java
+++ src/main/java/com/cenimarket/backend/listing/dto/response/ListingsListResponse.java
@@ -17,8 +17,8 @@ public class ListingsListResponse {
     private final Long id;
     private final String title;
     private final Integer price;
-    private final List<ListingImage> images;
-    private final Category category;
+    private final List<ListingImageResponse> images;
+    private final CategoryResponse category;
     private final Integer likeCount;
     private final LocalDateTime updatedAt;
 
@@ -27,8 +27,12 @@ public class ListingsListResponse {
                 .id(listing.getId())
                 .title(listing.getTitle())
                 .price(listing.getPrice())
-                .images(listing.getImages())
-                .category(listing.getCategory())
+                .images(
+                        listing.getImages().stream()
+                                .map(ListingImageResponse::from)
+                                .toList()
+                )
+                .category(CategoryResponse.from(listing.getCategory()))
                 .likeCount(listing.getLikeCount())
                 .updatedAt(listing.getUpdatedAt())
                 .build();
