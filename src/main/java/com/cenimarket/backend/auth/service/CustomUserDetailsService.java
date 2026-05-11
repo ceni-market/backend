7cab49615aa0cd8c62a27f78de77e2599b448a4a
Gorani77 <sp2877@naver.com>
2026-05-11 14:53:13 +0900

    feat: @AuthenticationPrincipal 통일

--- src/main/java/com/cenimarket/backend/auth/service/CustomUserDetailsService.java
+++ src/main/java/com/cenimarket/backend/auth/service/CustomUserDetailsService.java
@@ -1,9 +1,13 @@
 package com.cenimarket.backend.auth.service;
 
 import com.cenimarket.backend.auth.domain.UserPrincipal;
+import com.cenimarket.backend.global.error.BusinessException;
+import com.cenimarket.backend.global.error.ErrorCode;
 import com.cenimarket.backend.user.domain.User;
 import com.cenimarket.backend.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
