86e6af19686c5cb41d4e76e4d468e5f2c3cf230a
dookh99 <dookh1999@gmail.com>
2026-05-11 16:10:35 +0900

    feat: 채팅방 입장 시 기존 채팅 데이터 불러오도록 기능 추가.

--- src/main/java/com/cenimarket/backend/chat/controller/WebSocketController.java
+++ src/main/java/com/cenimarket/backend/chat/controller/WebSocketController.java
@@ -1,15 +1,11 @@
 package com.cenimarket.backend.chat.controller;
 
-import com.cenimarket.backend.chat.dto.request.ChatMessageSendRequest;
+import com.cenimarket.backend.chat.dto.ChatMessageDto;
 import com.cenimarket.backend.chat.service.ChatService;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class WebSocketController {
@@ -21,12 +17,11 @@ public class WebSocketController {
         this.messageSendingOperations = messageSendingOperations;
     }
 
-
     /* config(WebSocketConfig.java)에 설정한 바와 같이 /publish 주소로 오는 요청은 여기로 라우팅된다.
     @DestinationVariable은 @MessageMapping과 함께 사용된다.
     @MessageMapping으로 전달되는 데이터의 목적지 주소를 가져온다. */
     @MessageMapping("/{roomId}")
-    public void sendMessage(@DestinationVariable Long roomId, ChatMessageSendRequest messageSendRequest) {
+    public void sendMessage(@DestinationVariable Long roomId, ChatMessageDto messageSendRequest) {
         chatService.saveMessage(roomId, messageSendRequest);
         //@SendTo 어노테이션으로도 할 수 있으나, 어노테이션을 이용하면 코드의 유연성이 떨어지기 때문에 따로 구현.
         messageSendingOperations.convertAndSend("/topic/" + roomId, messageSendRequest);
