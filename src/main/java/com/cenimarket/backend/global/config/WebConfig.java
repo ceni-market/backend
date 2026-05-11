2bf82e3a12152d446f559a588280fb909ae71c24
AJH <AJH@>
2026-05-11 11:43:33 +0900

    feat: 이미지 업로드 > 로컬 저장 > 이미지 경로반환 추가

---
+++ src/main/java/com/cenimarket/backend/global/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.cenimarket.backend.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // /uploads/images/** 요청을 프로젝트 루트의 uploads/images 폴더와 연결한다.
+        registry.addResourceHandler("/uploads/images/**")
+                .addResourceLocations("file:uploads/images/");
+    }
+}
