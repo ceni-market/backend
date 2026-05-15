e0d49fd16534cd5f570daeee3c0f8f97587ba174
Gorani77 <sp2877@naver.com>
2026-05-15 14:04:35 +0900

    feat: 모바일 - 로그인 페이지

--- src/main/java/com/cenimarket/backend/mobile/controller/MobilePageController.java
+++ src/main/java/com/cenimarket/backend/mobile/controller/MobilePageController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MobilePageController {
-    @GetMapping("/login")
+    @GetMapping("/mobile/login")
     public String loginPage() {
         return "auth/login";
     }
