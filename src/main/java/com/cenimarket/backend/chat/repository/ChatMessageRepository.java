39dacf99fa91e37cf4afbe95ce17b1751a8cb454
doo <dookh1999@kyonggi.ac.kr>
2026-05-26 00:59:20 +0900

    feat: 모바일 채팅방 조회 시 읽지 않은 메시지 개수 출력 및 같은 유저들끼리 다른 게시글로 다시 채팅할 경우 게시글 Id 변경.

--- src/main/java/com/cenimarket/backend/chat/repository/ChatMessageRepository.java
+++ src/main/java/com/cenimarket/backend/chat/repository/ChatMessageRepository.java
@@ -2,11 +2,16 @@ package com.cenimarket.backend.chat.repository;
 
 import com.cenimarket.backend.chat.domain.ChatMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
     List<ChatMessage> findByChatRoomIdOrderByCreatedAtAsc(Long chatRoom);
+
+    @Query("select count(*) from ChatMessage cm where cm.createdAt > :lastReadAt")
+    int countUnreadMessage(LocalDateTime lastReadAt);
 }
